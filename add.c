3f2b3b258ec61d3ae228b66eb0cc68695816c5aa
Nikhil-Priyadarshi <72985481+Nikhil-Priyadarshi@users.noreply.github.com>
2020-10-16 21:28:21 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a,b,sum;
+  clrscr();
+  printf("Enter Two Numbers:");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("Sum of %d and %d = %d",a,b,sum);
+  getch();
+}
