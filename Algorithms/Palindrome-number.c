61bc5964f0d806c056b5b82006d1e3ea403a4cc6
Arindam Das <92679140+arindamdas13@users.noreply.github.com>
2021-10-25 21:31:40 +0530

    Create Palindrome-number.c

---
+++ Algorithms/Palindrome-number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>  
+int main()    
+{    
+int n,r,sum=0,temp;    
+printf("enter a number=");    
+scanf("%d",&n);    
+temp=n;    
+while(n>0)    
+{    
+r=n%10;    
+sum=(sum*10)+r;    
+n=n/10;    
+}    
+if(temp==sum)    
+printf("This is a palindrome number ");    
+else    
+printf("This is not a palindrome number");   
+return 0;  
+} 
