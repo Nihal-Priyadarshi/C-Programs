dfc987dbfbe71ef7716e9ca07be64844496b3668
Om Mehrotra <97609607+MehrotrajiWebsiting@users.noreply.github.com>
2022-10-20 22:54:11 +0530

    Create FactorialUsingRecursion.c
    C program to find factorial of a number given by the user using recursion.

---
+++ FactorialUsingRecursion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int fact(int n);
+int fact(int n){
+  if(n<=0) return 1;
+  else return n*fact(n-1);
+}
+int main(){
+  int n;
+  
+  printf("ENTER NUMBER:\n");
+  scanf("%d",&n);
+  
+  printf("FACTORIAL IS : %d",fact(n));
+}
